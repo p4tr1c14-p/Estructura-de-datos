be7448285964e11aed92043cf421397824ebeb9f
Patricia Pérez Cruz <163355108+p4tr1c14-p@users.noreply.github.com>
2024-10-10 15:51:25 -0600

    Agregue 2 archivos. 10/10/24

---
+++ Fundamentos-Python/Hola_mundo.py
@@ -0,0 +1,8 @@
+#PATRICIA PÉREZ CRUZ
+#21 DE OCTUBRE DE 2024
+#DOCUMENTO DE INICIALIZACIÓN PARA EL CURSO DE PYTHON SOBRE COMO SE PUEDE IMPRIMIR ALGO EN
+#LA CONSOLA
+
+print("Hola mundo")
+"""La función "print" sirve para mostrar algo en la pantalla y a comparación de C 
+aquí  no se necesita el ";" al final y se escribe sin la "f" en el final"""
