747f701b470f4b0a809ef435035a791f89805cd7
Patricia Pérez Cruz <163355108+p4tr1c14-p@users.noreply.github.com>
2024-10-10 16:53:08 -0600

    Agregue 2 archivos. 10/10/24.

--- Fundamentos-Python/Hola_mundo.py
+++ Fundamentos-Python/Hola_mundo.py
@@ -1,8 +1,20 @@
 #PATRICIA PÉREZ CRUZ
-#21 DE OCTUBRE DE 2024
-#DOCUMENTO DE INICIALIZACIÓN PARA EL CURSO DE PYTHON SOBRE COMO SE PUEDE IMPRIMIR ALGO EN
-#LA CONSOLA
+#7 DE OCTUBRE DE 2024
+#ESTE ES UN DOCUMENTO DE INICIALIZACIÓN PARA EL CURSO DE PYTHON SOBRE COMO SE PUEDE IMPRIMIR ALGO EN
+#LA CONSOLA, POR LO QUE A CONTINUACIÓN EXPONDRE MIS COMENTARIOS ACERCA DE LO NUEVO QUE APRENDI
 
-print("Hola mundo")
-"""La función "print" sirve para mostrar algo en la pantalla y a comparación de C 
-aquí  no se necesita el ";" al final y se escribe sin la "f" en el final"""
+# EL-> PRINT: ESTE COMANDO NOS AYUDA PARA PODER IMPRIMIR ALGO EN NUESTRAS PANTALLAS Y ADEMÁS
+# REPRESENTA TAMBIÉN UN SALTO DE PÁGINA POR SI SOLO. EL PRINT ES UNO DE
+# LOS COMANDO MÁS BÁSICOS QUE SE DEBEN DE APRENDER PARA PODER UTILIZAR PYTHON
+
+#EJEMPLO PARA IMPRIMIR:
+
+print()    #->SALTO DE PÁGINA
+print("Hola mundo")   #->PARA IMPRIMIR UN CARTEL
+
+
+# CONCLUSIÓN:
+""" La función "print" sirve para mostrar algo en la pantalla de salida o ser un
+salto de página y a comparación de  lengujes de programamción como C o C++  aquí  
+no se necesita poner un ";" al final para que no marque error y además se escribe 
+sin la "f" en el final ya que en C así se hacia"""
