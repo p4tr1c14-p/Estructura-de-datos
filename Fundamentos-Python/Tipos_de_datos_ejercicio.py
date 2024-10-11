56f0fffc9e8670e4f8db4078b5cca62311ce3e45
Patricia Pérez Cruz <163355108+p4tr1c14-p@users.noreply.github.com>
2024-10-11 16:30:40 -0600

    Agregue 2 archivos más- 11/10/24.

---
+++ Fundamentos-Python/Tipos_de_datos_ejercicio.py
@@ -0,0 +1,44 @@
+"""
+Patricia Pérez Cruz
+11 de octubre de 2024.
+Descripción:
+Mi primer ejercicio utilizando los tipos básico de datos en Python.
+"""
+
+
+dia = "Lunes"
+pasaje = 20
+comida = 135.5
+otros = 34.5
+fue_mayor = True
+
+# Aqui voy a imprimir las variables, añadiendo información adicional para comprender lo que se imprime
+
+print()
+print("*** GASTOS DIARIOS ***")
+print()
+print("Día: ", dia)
+print("Pasaje: ", pasaje)
+print("Comida: ", comida)
+print("Otros gastos: ", otros)
+print("¿Fue mayor a mi presupuesto?:", fue_mayor)
+print()
+
+
+# Aqui voy a cambiar los datos para el dia Martes
+
+dia = "Martes"
+pasaje = 12
+comida = 45.5
+otros = 4.5
+fue_mayor = False
+
+# Ahora voy a volver a imprimir pero con los datos ya modificados
+
+print("*** GASTOS DIARIOS ***")
+print()
+print("Día: ", dia)
+print("Pasaje: ", pasaje)
+print("Comida: ", comida)
+print("Otros gastos: ", otros)
+print("¿Fue mayor a mi presupuesto:", fue_mayor)
