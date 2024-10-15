7907f402ee22f7be9e50f59801ad0938bd0a5f47
Patricia Pérez Cruz <163355108+p4tr1c14-p@users.noreply.github.com>
2024-10-15 15:13:46 -0600

    Agregue 1 archivo máss.- 15/10/24

---
+++ Fundamentos-Python/Entrada_consola_ejercicio.py
@@ -0,0 +1,16 @@
+"""
+ENTRADA CONSOLA EJERCICIO
+
+a) Pida 2 números decimales por consola al usuario utilizando la función input.
+
+b) Muestre los resultados de las operaciones básicas con esos números: suma, resta, multiplicación y división.
+
+Nota: Asuma que el usuario siempre va a ingresar números y que el segundo número es diferente de cero.
+"""
+
+print()
+var_float1 = input("Ingrese un número decimal: ")
+var_float2 = input("Ingrese otro número decimal: ")
+
+
+print("*** SUMA ***")
