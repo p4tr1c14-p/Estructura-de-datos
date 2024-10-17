99768a495a8ca8221b26a08ffdddc269d2734d26
Patricia Pérez Cruz <163355108+p4tr1c14-p@users.noreply.github.com>
2024-10-17 17:27:04 -0600

    Agregue archivo máss.- 17/10/24

--- Fundamentos-Python/Entrada_consola_ejercicio.py
+++ Fundamentos-Python/Entrada_consola_ejercicio.py
@@ -12,5 +12,26 @@ print()
 var_float1 = input("Ingrese un número decimal: ")
 var_float2 = input("Ingrese otro número decimal: ")
 
+numero1 = float(var_float1)
+numero2 = float(var_float2)
 
 print("*** SUMA ***")
+suma = numero1 + numero2
+print(f"El resultado de la suma es = {suma:.2f}")
+print()
+
+print("*** RESTA ***")
+resta = numero1 - numero2
+print(f"El resultado de la resta es = {resta:.2f}")
+print()
+
+print("*** MULTIPLICACION ***")
+multiplicacion = numero1 * numero2
+print(f"El resultado de la multiplicación es = {multiplicacion:.2f}")
+print()
+
+print("*** DIVISIÓN ***")
+division = numero1 / numero2
+print(f"El resultado de la división es = {division:.2f}")
+print()
+
