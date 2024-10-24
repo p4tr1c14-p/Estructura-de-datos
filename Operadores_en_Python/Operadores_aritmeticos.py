115923c433a3cc0fa4162611c4201631f867ec8d
Patricia Pérez Cruz <163355108+p4tr1c14-p@users.noreply.github.com>
2024-10-24 15:31:04 -0600

    Agregue un archivo máss.- 24/10/24

--- Operadores_en_Python/Operadores_aritmeticos.py
+++ Operadores_en_Python/Operadores_aritmeticos.py
@@ -1,21 +1,46 @@
 
+'''
+Nombre:Patricia Pérez Cruz
+Fecha: 24 de octubre de 2024
+Descripción:
+Ejemplos de uso de los operadores relacionales.
+➡️  Los operadores relacionales son esenciales
+para la lógica de programación en Python, ya que
+permiten comparar y tomar decisiones basadas
+en los resultados de esas comparaciones
+'''
+
+"""
+Los operadores lógicos permiten combinar expresiones booleanas (verdadero o falso) para crear condiciones más complejas.
+Estos operadores nos permiten realizar operaciones lógicas como "y", "o" y "no", lo que nos da la capacidad de tomar 
+decisiones más sofisticadas dentro de nuestros programas.
+"""
+
+# Se solicita por consola que se ingresen dos valores (Si/No) para covnertirlas a expresiones booleanas.
+expresion1 = input("Ingresa un valor (Si/No): ")
+expresion2 = input("Ingresa otro valor (Si/No): ")
+
+# Las cadenas se convierten a expresiones booleanas (ver Fundamentos-Python -> Entrada_conversiones.py).
+expresion1 = expresion1.lower() == "si"
+expresion2 = expresion2.lower() == "si"
+
+# Se imprimen mensajes sobre operaciones lógicas.
+print(f"¿Ambas expresiones fueron 'si'? {expresion1 and expresion2}")
+print(f"¿Alguna expresión fue 'si'? {expresion1 or expresion2}")
+print(f"Lo contrario de la primera expresión es: {not expresion1}")
+print(f"Lo contrario de la segundo expresión es: {not expresion2}")
 
 
-# + SUMA
-# - RESTA
-# * MULTIPLICACIÓN
-# / DIVISIÓN
-# % MODULO
-
-
-var_int = input("Ingrese su número entero: ")
-var_int2 = input("Ingrese su otro número entero: ")
-
-entero1 = int(var_int)
-entero2 = int(var_int2)
-
-multi = entero1 / entero2
-print(f" El resultado: {multi: .2f}")
-print(entero1 // entero2)
-print(entero1 ** entero2)
+"""
+➡️ Los siguientes operadores aritméticos ya los conicía
+ + SUMA
+ - RESTA
+ * MULTIPLICACIÓN
+ / DIVISIÓN
+ % MODULO
+ 
+ ➡️ Pero estos dos fueron nuevos para mi 😀
+ // DIVISIÓN QUE SOLO ME DA EL RESULTADO ENTERO
+ ** ES PARA ELEVAR A UN NÚMERO (FUNCIONA COMO ESTE SIMBOLO "^")
+"""
 
