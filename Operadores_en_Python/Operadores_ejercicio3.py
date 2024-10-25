89c8c6dde87a37bd14c6b5655ac4faf6d6113259
Patricia Pérez Cruz <163355108+p4tr1c14-p@users.noreply.github.com>
2024-10-25 15:58:34 -0600

    Agregue un archivo máss.- 25/10/24

--- Operadores_en_Python/Operadores_ejercicio3.py
+++ Operadores_en_Python/Operadores_ejercicio3.py
@@ -1,5 +1,40 @@
+'''
+Nombre:Patricia Pérez Cruz
+Fecha: 25 de octubre de 2024
+Descripción:
+Tercer ejercicio para poner en práctica mis conocimientos adquiridos sobre
+los operadores lógicos.
+Este programa determinará si un usuario se autentifica correctamente con su usuario y contraseña.
+'''
 
-numero = input("Digite un numero: ")
-total = int(numero)
+#a) Internamente declare dos cadenas constantes, una para el usuario y otra para la contraseña.
+#➡️ Aquí declaro mis constantes las dos como cadenas para que no surjan errores 😀
+USUARIO_CONSTANTE = "Patricia"
+CONTRASEÑA_CONSTANTE = "1234"
 
-print(f"El numero {total} está entre 10 y 15: {total >= 10 and total <= 15} ")
+#b) Pida al usuario una cadena con el usuario.
+print(" *** SISTEMA DE AUTENTIFICACIÓN ***")
+print()
+usuario_entrada = input("Ingrese su usuario: ")
+
+#c) Pida al usuario una cadena con la contraseña.
+contraseña_entrada = input("Ingrese su contraseña: ")
+
+#d) Si ambas cadenas son iguales a las cadenas declaradas internamente, entonces el usario se autenticó correctamente.
+#⬆️ A continuación compararé las dos cadenas para saber si lo que ingresaron coincide con lo declarado internamente
+#pero para eso primero voy a convertirlos a booleanos
+
+usuario = usuario_entrada == USUARIO_CONSTANTE
+contraseña = contraseña_entrada == CONTRASEÑA_CONSTANTE
+
+#e) Muestre el resultado en consola como valor booleano (True/False).
+
+print(f"¿El acceso fue correcto?: {usuario and contraseña}")
+
+#d) Si ambas cadenas son iguales a las cadenas declaradas internamente, entonces el usario se autenticó correctamente.
+#⬆️ Lo anterior se va a comprobar si el en "print" el resultado es "TRUE" o "FALSE"
+
+
+"""
+Nota: Las cadenas no tiene que ser convertidas a minúsculas.
+"""
