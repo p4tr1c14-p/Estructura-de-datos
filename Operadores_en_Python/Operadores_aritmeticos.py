bb67daefcbfb587d127b99f9912a78789baa4785
Patricia Pérez Cruz <163355108+p4tr1c14-p@users.noreply.github.com>
2024-10-26 12:25:37 -0600

    Agregue un archivo máss.- 26/10/24

--- Operadores_en_Python/Operadores_aritmeticos.py
+++ Operadores_en_Python/Operadores_aritmeticos.py
@@ -4,10 +4,6 @@ Nombre:Patricia Pérez Cruz
 Fecha: 24 de octubre de 2024
 Descripción:
 Ejemplos de uso de los operadores relacionales.
-➡️  Los operadores relacionales son esenciales
-para la lógica de programación en Python, ya que
-permiten comparar y tomar decisiones basadas
-en los resultados de esas comparaciones
 '''
 
 """
@@ -16,31 +12,46 @@ Estos operadores nos permiten realizar operaciones lógicas como "y", "o" y "no"
 decisiones más sofisticadas dentro de nuestros programas.
 """
 
-# Se solicita por consola que se ingresen dos valores (Si/No) para covnertirlas a expresiones booleanas.
-expresion1 = input("Ingresa un valor (Si/No): ")
-expresion2 = input("Ingresa otro valor (Si/No): ")
-
-# Las cadenas se convierten a expresiones booleanas (ver Fundamentos-Python -> Entrada_conversiones.py).
-expresion1 = expresion1.lower() == "si"
-expresion2 = expresion2.lower() == "si"
-
-# Se imprimen mensajes sobre operaciones lógicas.
-print(f"¿Ambas expresiones fueron 'si'? {expresion1 and expresion2}")
-print(f"¿Alguna expresión fue 'si'? {expresion1 or expresion2}")
-print(f"Lo contrario de la primera expresión es: {not expresion1}")
-print(f"Lo contrario de la segundo expresión es: {not expresion2}")
-
-
 """
-➡️ Los siguientes operadores aritméticos ya los conicía
+➡️ Los siguientes operadores aritméticos ya los conocía:
  + SUMA
  - RESTA
  * MULTIPLICACIÓN
  / DIVISIÓN
  % MODULO
- 
- ➡️ Pero estos dos fueron nuevos para mi 😀
+ ➡️ Pero estos dos fueron nuevos para mí 😀
  // DIVISIÓN QUE SOLO ME DA EL RESULTADO ENTERO
- ** ES PARA ELEVAR A UN NÚMERO (FUNCIONA COMO ESTE SIMBOLO "^")
+ ** ES PARA ELEVAR EL PRIMER NÚMERO A LA PONTENCIA DEL SEGUNDO NÚMERO
 """
 
+# Se solicitan dos números enteros al usuario.
+numero1 = int(input("Ingresa un número entero: "))
+numero2 = int(input("Ingresa otro número entero: "))
+
+# Se realizan las operaciones aritméticas.
+print()
+print("  ***   Ejemplos de uso de los operadores aritméticos   ***")
+
+print(f"La suma de ({numero1} + {numero2}) es: {numero1 + numero2}") #➡️ Realiza la suma de los dos números
+print(f"La resta de ({numero1} - {numero2}) es: {numero1 - numero2}") #➡️ Realiza la resta de los dos números
+print(f"La multiplicación de ({numero1} * {numero2}) es: {numero1 * numero2}") #➡️ Multiplica los dos números
+print(f"La división de ({numero1} / {numero2}) es: {(numero1 / numero2):.2f}")  #➡️ Muestra la división con dos decimales (Notar la forma para mostrar dos decimales)
+print(f"La división entera de ({numero1} // {numero2}) es: {numero1 // numero2}") #➡️ Realiza la división entera
+print(f"El módulo de ({numero1} % {numero2}) es: {numero1 % numero2}") #➡️ Calcula el residuo de la división
+print(f"La exponenciación  de ({numero1} ** {numero2}) es: {numero1 ** numero2}") #➡️ Eleva el primer número a la potencia del segundo
+
+
+"""
+Ejercicio corto para repasar los conocimientos anteriores:
+
+var_int = input("Ingrese su número entero: ")
+var_int2 = input("Ingrese su otro número entero: ")
+
+entero1 = int(var_int)
+entero2 = int(var_int2)
+
+multi = entero1 / entero2
+print(f" El resultado: {multi: .2f}")
+print(entero1 // entero2)
+print(entero1 ** entero2)
+"""
