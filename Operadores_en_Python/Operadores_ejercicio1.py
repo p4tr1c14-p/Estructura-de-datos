8b50febac23c63bb96ddb7ab720650de85f43cd3
Patricia Pérez Cruz <163355108+p4tr1c14-p@users.noreply.github.com>
2024-10-26 14:19:18 -0600

    Agregue un archivo máss.- 26/10/24

--- Operadores_en_Python/Operadores_ejercicio1.py
+++ Operadores_en_Python/Operadores_ejercicio1.py
@@ -3,7 +3,7 @@ Nombre:Patricia Pérez Cruz
 Fecha: 24 de octubre de 2024
 Descripción:
 Ejercicio para poner en práctica mis conocimientos adquiridos sobre
-los operadores lógicos
+los operadores relacionales
 '''
 
 print(" *** SISTEMA DE BONIFICACIÓN *** ")
@@ -11,7 +11,7 @@ print(" *** SISTEMA DE BONIFICACIÓN *** ")
 #a) Solicite al usuario un número decimal con el valor de una compra.
 cantidad = input("¿Compras mayores a $5,000.00? Ingrese su cantidad con decimales: ")
 
-#b) Pregunte al usuario si la compra fue a MSI (Si/No)
+#b) Pregunte al usuario si la compra fue a MSI (Si/No).
 cadena = input("¿Compras a MSI? (si/no): ")
 
 total = float(cantidad) #➡️ Convertí a float
@@ -22,4 +22,4 @@ cadena = cadena.lower() == "si" #➡️ Aquí con solo esta línea de código lo
 
 #d) Muestre el resultado en consola como valor booleano (True/False).
 print(f"¿Aplica bonificación?: {total>=5000 and cadena}") #➡️ Aquí comparo que las dos condiciones se deben de cumplir
-#para que así me de "TRUE".
+#para que así me de "TRUE"
