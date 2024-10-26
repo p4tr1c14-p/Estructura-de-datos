042215bfbb1581c4e6981a5d177e4578974563e2
Patricia Pérez Cruz <163355108+p4tr1c14-p@users.noreply.github.com>
2024-10-26 14:34:37 -0600

    Agregue un archivo máss.- 26/10/24

--- Operadores_en_Python/Operadores_ejercicio4.py
+++ Operadores_en_Python/Operadores_ejercicio4.py
@@ -10,10 +10,8 @@ Este programa determinará True o False de acuerdo a la expresión (exp1 O exp2)
 print(" *** Expresión booleana (exp1 O exp2) Y (exp3 O exp4) ***")
 print()
 #a) Pida al usuario cuatro valores booleanos (V/F).
-valor_uno = input("Ingrese si o no: ") #➡️ Aquí solicito si o no (El "si" para mí es mi "V" y el "no" es mi "F")
-valor_dos = input("Ingrese si o no: ")
-valor_tres = input("Ingrese si o no: ")
-valor_cuatro = input("Ingrese si o no: ")
+#➡️ Aquí solicito si o no (El "si" para mí es mi "V" y el "no" es mi "F")
+valor_uno, valor_dos, valor_tres, valor_cuatro = input("Ingrese si o no: "), input("Ingrese si o no: "), input("Ingrese si o no: "), input("Ingrese si o no: ")
 
 #b) Obtenga el resultado de la expresión booleana de acuerdo con los valores ingresados.
 valor_one = valor_uno.lower() == "si" #➡️ Convierto la cadena de antes a minúsculas y las comparo para que, si es otro valor que no sea  "si", me dé "FALSE"
@@ -24,4 +22,3 @@ valor_four = valor_cuatro.lower() == "si"
 #c) Muestre el resultado en consola como valor booleano (True/False).
 
 print(f"El resultado de la expresión booleana es: {(valor_one or valor_two) and (valor_three or valor_four)}") #➡️ En una sola línea hago las dos comparaciones
-
