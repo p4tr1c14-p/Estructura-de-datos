b7d6f5150121131f52e3d3df1bbb6f8a93eca001
Patricia Pérez Cruz <163355108+p4tr1c14-p@users.noreply.github.com>
2024-10-26 13:02:24 -0600

    Agregue un archivo máss.- 26/10/24

--- Operadores_en_Python/Operadores_ejercicio4.py
+++ Operadores_en_Python/Operadores_ejercicio4.py
@@ -10,24 +10,22 @@ Este programa determinará True o False de acuerdo a la expresión (exp1 O exp2)
 print(" *** Expresión booleana (exp1 O exp2) Y (exp3 O exp4) ***")
 print()
 #a) Pida al usuario cuatro valores booleanos (V/F).
-valor_uno = input("Ingrese si o no: ")
+valor_uno = input("Ingrese si o no: ")  #➡️ Se pide al usuario que ingrese "si" o "no" (Para mí el "si" es mi "V" y el "no" es mi "F")
 valor_dos = input("Ingrese si o no: ")
 valor_tres = input("Ingrese si o no: ")
 valor_cuatro = input("Ingrese si o no: ")
 
 #b) Obtenga el resultado de la expresión booleana de acuerdo con los valores ingresados.
-valor_one = valor_uno.lower() == "si"
+valor_one = valor_uno.lower() == "si" #➡️ Convertí la entrada a booleano: True si es "si", para que cualquier otra palabra aperte del "si" me de "FALSE"
 valor_two = valor_dos.lower() == "si"
 valor_three = valor_tres.lower() == "si"
 valor_four = valor_cuatro.lower() == "si"
 
+#➡️Aquí evalué las dos operaciones lógicas
+operacion_uno = valor_one or valor_two
+operacion_dos = valor_three or valor_four
 
-"""
-Este programa determinará True o False de acuerdo a la expresión (exp1 O exp2) Y (exp3 O exp4). Para ello:
+#c) Muestre el resultado en consola como valor booleano (True/False).
+print(f"El resultado de la expresión boolena es: {operacion_uno and operacion_dos}")
 
-a) Pida al usuario cuatro valores booleanos (V/F).
 
-b) Obtenga el resultado de la expresión booleana de acuerdo con los valores ingresados.
-
-c) Muestre el resultado en consola como valor booleano (True/False).
-"""
