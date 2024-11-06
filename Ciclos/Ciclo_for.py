0c2d3850a2bf84999c537a0e9860468ef9be25f2
Patricia Pérez Cruz <163355108+p4tr1c14-p@users.noreply.github.com>
2024-11-06 15:55:31 -0600

    Subí 2 archivos, 6 de noviembre de 2024

---
+++ Ciclos/Ciclo_for.py
@@ -0,0 +1,23 @@
+"""
+cadena = input("Ingrese una cadena: ")
+
+contador= 0
+
+for caracter in cadena:
+    contador = contador + 1
+    print(caracter, end=" ")
+
+print()
+print(contador)
+
+"""
+
+"""
+alumnos = ["Rosalinda", "Juan", "Lourdes", "Tania", "Bryan", "Rebeca", "Jennifer", "Héctor", "Galilea", "Patricia", "Jesús", "Addi"]
+for alumno in alumnos:
+    print(alumno)
+    print(f"Hola {alumno}")
+"""
+
+for i in range(1,10):
+    print(i, end=" ")
--- Fundamentos-Python/Pruebas_clase.py
+++ Fundamentos-Python/Pruebas_clase.py
@@ -1,30 +1,12 @@
-# Ejercicio4_juego_adivinador.py
+#Genera un número aleatorio y hz que lo adivinen una solo vez.
+from random import randint
 
-from random import randint  # ➡️ Importé la función randint para generar un número aleatorio
+numeros=randint(1,50)
+variable=0
 
-# Inicializo el número secreto
-numero_secreto = randint(1, 100)  # ➡️ Genero un número aleatorio entre 1 y 100
-intentos = 5  # ➡️ Establezco el número de intentos permitidos
-intento_actual = 1  # ➡️ Inicializo el contador de intentos
-
-print("¡Bienvenido al juego de adivinanza! 🎉")  # ➡️ Mensaje de bienvenida
-print("Tienes 5 intentos para adivinar un número entre 1 y 100.")  # ➡️ Indico al jugador cuántos intentos tiene
-
-# Inicio del bucle de intentos
-while intento_actual <= intentos:  # ➡️ Mientras no se superen los intentos
-    adivina = int(input(f"Número de intento {intento_actual}. Ingresa un número de 1-100: "))  # ➡️ Indico el intento actual y solicito el número
-
-# Verifico si el número ingresado es correcto
-    if adivina == numero_secreto:  # ➡️ Compruebo si el número ingresado es igual al número secreto
-        print(f"¡Felicidades! Adivinaste el número {numero_secreto} en {intento_actual} intentos.")  # ➡️ Mensaje de felicitación
-        break  # ➡️ Salgo del bucle si el jugador adivina correctamente
-    elif adivina < numero_secreto:  # ➡️ Si el número ingresado es menor que el número secreto
-        print("El número a adivinar es mayor.")  # ➡️ Indico que el número es mayor
-    else:  # ➡️ Si el número ingresado es mayor que el número secreto
-        print("El número a adivinar es menor.")  # ➡️ Indico que el número es menor
-
-    intento_actual += 1  # ➡️ Incremento el contador de intentos
-
-# Si el jugador no adivina el número después de los 5 intentos
-if adivina != numero_secreto:  # ➡️ Verifico si el número adivinado no es el secreto
-    print(f"Perdiste. El número era: {numero_secreto}.")  # ➡️ Mensaje con el número secreto
+while variable!=numeros:
+    variable=input("que número cres que es?:")
+    if variable==numeros:
+        print("El numero es el correcto")
+    else:
+        print(f"El numero es:{numeros}")
