f13f97b83b2bee0fc493c1b007a2f041725ed4ef
patricia <patriciaperezcruz007@gmail.com>
2024-12-04 20:53:22 -0600

    Subí 1 archivo(s), 4 de diciembre de 2024
    (cherry picked from commit fe6f1cd168c70e535d31936f6d043b315e8d763c)

--- Funciones-y-Colectores/Diccionarios_ej1_piedra_papel_tijera.py
+++ Funciones-y-Colectores/Diccionarios_ej1_piedra_papel_tijera.py
@@ -6,8 +6,9 @@ Este programa es una nueva versión del juego
 realizado de piedra, papel y tijeras, pero
 utilizando un diccionario para las reglas del juego.
 """
-from random import choice
+from random import choice #➡️ Siempre importar porque si no me marca error
 
+#➡️ Estas son las constantes para representar las opciones del juego y los posibles resultados
 PIEDRA = "Piedra"
 PAPEL = "Papel"
 TIJERAS = "Tijeras"
@@ -36,6 +37,7 @@ def jugar(opcion):
     eleccion_cpu = choice([PIEDRA, PAPEL, TIJERAS])
     return eleccion_usuario, eleccion_cpu
 
+#➡️ Este es mi diccionario que define las reglas del juego: las combinaciones ganadoras para el jugador o la CPU
 piedra_papel_tijeras = {(PIEDRA, TIJERAS): JUGADOR,
                         (PIEDRA, PAPEL): CPU,
                         (TIJERAS, PAPEL): JUGADOR,
@@ -43,6 +45,10 @@ piedra_papel_tijeras = {(PIEDRA, TIJERAS): JUGADOR,
                         (PAPEL, PIEDRA): JUGADOR,
                         (PAPEL, TIJERAS): CPU
                         }
+#➡️ Variables para llevar el conteo de victorias
+vi_ju = 0
+vi_cpu = 0
+vi_empate = 0
 
 print(" ***  Juego de piedra, papel o tijeras  *** ")
 opcion = None
@@ -56,18 +62,30 @@ while opcion != 0:
             print("Gracias por jugar. ¡Hasta pronto!")
             break
 
+        #➡️ Aquí se obtienen las elecciones del jugador y de la CPU
+
         eleccion_usuario, eleccion_cpu = jugar(opcion)
         print(f"\nJugador eligió: {eleccion_usuario}")
         print(f"CPU eligió: {eleccion_cpu}")
         print()
-
+        #➡️ Se determinó el resultado del juego con base en las reglas
         resultado = piedra_papel_tijeras.get((eleccion_usuario, eleccion_cpu), EMPATE)
+
         if resultado == JUGADOR:
+            vi_ju = vi_ju + 1
             print("El ganador es el Jugador")
+            #➡️ Muestro los resultados acumulados
+            print(f"Victoras del jugador: {vi_ju}, Victorias del CPU: {vi_cpu}, Empates: {vi_empate}")
             print()
         elif resultado == CPU:
+            vi_cpu = vi_cpu + 1
             print("El ganador es el CPU")
+            # ➡️ Muestro los resultados acumulados
+            print(f"Victoras del jugador: {vi_ju}, Victorias del CPU: {vi_cpu}, Empates: {vi_empate}")
             print()
         else:
+            vi_empate = vi_empate + 1
             print("Es un empate")
+            # ➡️ Muestro los resultados acumulados
+            print(f"Victoras del jugador: {vi_ju}, Victorias del CPU: {vi_cpu}, Empates: {vi_empate}")
             print()
