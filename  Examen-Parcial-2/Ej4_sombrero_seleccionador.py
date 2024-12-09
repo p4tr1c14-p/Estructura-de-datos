0ebbd3b74a6d1664d7e0ed6042950e82141aa446
patricia <patriciaperezcruz007@gmail.com>
2024-12-08 23:34:29 -0600

    Subí 1 archivo(s), 6 de diciembre de 2024
    (cherry picked from commit fe6f1cd168c70e535d31936f6d043b315e8d763c)

---
+++  Examen-Parcial-2/Ej4_sombrero_seleccionador.py
@@ -0,0 +1,8 @@
+"""
+Nombre: Patricia Pérez Cruz
+Fecha: 6 de diciembre de 2024
+Descripción:
+Este programa convierte un texto al lenguaje hacker (lenguaje leet o 1337).
+Esta escritura se caracteriza por reemplazar las letras por números y símbolos.
+"""
+import random
