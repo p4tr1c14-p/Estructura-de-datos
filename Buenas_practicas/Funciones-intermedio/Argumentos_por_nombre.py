1b46bdd319165b64939b97730e2df9ad93a6fdc7
Patricia Pérez Cruz <163355108+p4tr1c14-p@users.noreply.github.com>
2025-01-16 15:45:53 -0600

    Subí 1 archivo(s), 15 de enero de 2025

--- Buenas_practicas/Funciones-intermedio/Argumentos_por_nombre.py
+++ Buenas_practicas/Funciones-intermedio/Argumentos_por_nombre.py
@@ -1,5 +1,30 @@
 
-def imprimir_alumno(nombre: str, edad: int, matr: int, grupo: int, semestre: int) -> None:
+
+
+def imprimir_alumno(nombre: str= "Hola", edad: int = 20, matr: int= 1243, grupo: int = 9, semestre: int= 8) -> None:
+    print(f"Datos personales: \n nombre: {nombre} \n edad: {edad} \n matricula: {matr} \n grupo: {grupo} \n semestre: {semestre} " )
+
+def main ()-> None:
+
+    NOMBRE = "PATRICIA"
+    EDAD = 19
+    MATRICULA = 12345
+    GRUPO = 303
+    SEMESTRE = 3
+
+
+    imprimir_alumno(semestre= SEMESTRE)
+    #imprimir_alumno(nombre="Sofia", matr=456, edad=20, semestre=3)
+    #imprimir_alumno(nombre="Sofia", 31, 123, grupo= 303, semestre= 3)
+
+
+if __name__ == '__main__':
+        main()
+
+
+"""
+
+def imprimir_alumno(nombre: str, edad: int, matr: int, grupo: int, semestre: int = 3) -> None:
     print(f"Datos personales: \n nombre: {nombre} \n edad: {edad} \n matricula: {matr} \n grupo: {grupo} \n semestre: {semestre} " )
 
 def main ()-> None:
@@ -7,10 +32,12 @@ def main ()-> None:
     EDAD = 19
     MATRICULA = 12345
     GRUPO = 303
-    SEMESTRE = 3
+    
 
-    imprimir_alumno(NOMBRE, EDAD, MATRICULA, GRUPO, SEMESTRE)
+    imprimir_alumno(NOMBRE, EDAD, MATRICULA, GRUPO)
 
 
 if __name__ == '__main__':
         main()
+
+"""
